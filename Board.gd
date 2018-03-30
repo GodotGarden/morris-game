84ac4eb28005962012b86fb288fc3f29906d432a
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-03-30 11:47:40 +0300

    Cleanup

--- Board.gd
+++ Board.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+# Let listeners know when the player moves
 signal player_moved
 
-func _get_player_turn():
-	return $Game.get_player_turn()
+# Set piece counters to zero
+var white_pieces_on_board = 0
+var black_pieces_on_board = 0
 
 func _on_Position_player_moved():
+	# Let listeners know the player moved
 	emit_signal("player_moved")
