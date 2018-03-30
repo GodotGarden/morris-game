3faa8e17b46ac9e04f0b25544d7718f9d8a364a1
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-03-30 11:21:36 +0300

    Move piece placement into own function

--- Position.gd
+++ Position.gd
@@ -17,22 +17,21 @@ func _ready():
 	Board = self.get_parent()
 	Game = Board.get_parent()
 
-
 func _on_Position_input_event(viewport, event, shape_idx):
 	# Check if the event was a mouse click
 	if (event is InputEventMouseButton && event.pressed):
-		# Check what player's turn it is
-		if Game.player_turn == "black":
-			# Put a black piece on the position
-			occupied_by = "black"
-			$Piece.set_texture(black_piece_texture)
-			
-			# Tell the board the player moved
-			emit_signal("player_moved")
-		elif Game.player_turn == "white":
-			# Put a white piece on the position
-			occupied_by = "white"
-			$Piece.set_texture(white_piece_texture)
-			
-			# Tell the board the player moved
-			emit_signal("player_moved")
+		place_current_player_piece()
+
+func place_current_player_piece():
+	# Check what player's turn it is
+	if Game.player_turn == "black":
+		# Put a black piece on the position
+		occupied_by = "black"
+		$Piece.set_texture(black_piece_texture)
+	elif Game.player_turn == "white":
+		# Put a white piece on the position
+		occupied_by = "white"
+		$Piece.set_texture(white_piece_texture)
+	
+	# Tell the board the player moved
+	emit_signal("player_moved")
