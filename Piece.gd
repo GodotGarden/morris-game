30a52388b2e9df69f924d022e6be3783491336a5
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-04-01 13:41:08 +0300

    Remove redundant code

--- Piece.gd
+++ Piece.gd
@@ -2,15 +2,19 @@ extends Control
 
 export var color = 'black'
 
+var piece_texture
+
 # Piece textures
 var black_piece_texture = load("res://images/black_piece.svg")
 var white_piece_texture = load("res://images/white_piece.svg")
 
 func _ready():
 	if color == 'black':
-		$Sprite.set_texture(black_piece_texture)
+		piece_texture = black_piece_texture
 	elif color == 'white':
-		$Sprite.set_texture(white_piece_texture)
+		piece_texture = white_piece_texture
+	
+	$Sprite.set_texture(piece_texture)
 
 func get_drag_data(position):
 	print(position)
