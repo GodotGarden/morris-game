488889923f71604db1e91514db537885c3544d94
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-04-01 13:47:38 +0300

    Change drag color to match piece

--- Piece.gd
+++ Piece.gd
@@ -3,25 +3,35 @@ extends Control
 export var color = 'black'
 
 var piece_texture
+var drag_preview
 
 # Piece textures
 var black_piece_texture = load("res://images/black_piece.svg")
 var white_piece_texture = load("res://images/white_piece.svg")
 
 func _ready():
+	var drag_preview_color
+	
 	if color == 'black':
 		piece_texture = black_piece_texture
+		drag_preview_color = Color(0,0,0)
 	elif color == 'white':
 		piece_texture = white_piece_texture
+		drag_preview_color = Color(1,1,1)
 	
 	$Sprite.set_texture(piece_texture)
+	
+	# Using a color picker button for drag preview
+	# TODO: use a Piece Sprite instead
+	drag_preview = ColorPickerButton.new()
+	drag_preview.color = Color(drag_preview_color)
+	drag_preview.rect_size = Vector2(50, 50)
 
 func get_drag_data(position):
 	print(position)
 	#self.visible = false
 	# Use another colorpicker as drag preview
-	var cpb = ColorPickerButton.new()
-	cpb.rect_size = Vector2(50, 50)
-	set_drag_preview(cpb)
+	
+	set_drag_preview(drag_preview)
 	# Return color as drag data
 	return self
